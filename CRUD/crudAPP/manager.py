753e669b9ed87084b32eabd87ea08c9b9390ae1d
pooriya <adibradp@gmail.com>
2024-08-06 18:54:07 +0330

    the User manager be created

---
+++ CRUD/crudAPP/manager.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """
+    the manager class inherits from BaseUserManager
+    this class created for build new user and save it
+    """
+    def create_user(self, email, username, password=None):
+        """
+        Creates and saves a new user
+        :param email: primary email
+        :param username: primary username
+        :param password: password field
+        :return: the created user
+        """
+        if not email and not username:
+            raise ValueError('Users must have an email address and username')
+
+        user = self.model(email=email, username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a new superuser
+        :param email: primary email
+        :param password: password field
+        :return: created superuser
+        """
+        user = self.model(email=email, username=email)
+
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
