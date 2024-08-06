b496bb7dedad82ab0b8f63ea3bc9f73d4aaf772c
pooriya <adibradp@gmail.com>
2024-08-06 18:53:44 +0330

    the User models be created

--- CRUD/crudAPP/models.py
+++ CRUD/crudAPP/models.py
@@ -1,2 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .manager import UserManager
 
+
+class User(AbstractBaseUser):
+    """
+    the primary user model for CRUD app and
+    override it from base User model
+    """
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
